60a598ef31398189661f0b16975f9b8d0437bcf1
gokulrameshd <gorad364@gmail.com>
2020-10-31 20:20:21 +0530

    final commit 4

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,11 +1,11 @@
-from .cos import *
-from .sin import *
-from .ReLU import *
-from .softmax import *
-from .tan import *
-from .e import *
-from .tanh import *
-from .sigmoid import *
-from .log import *
+from calculator.cos import *
+from calculator.sin import *
+from calculator.ReLU import *
+from calculator.softmax import *
+from calculator.tan import *
+from calculator.e import *
+from calculator.tanh import *
+from calculator.sigmoid import *
+from calculator.log import *
 
 # __all__ = (cos.__all__+sin.__all__+ReLU.__all__+softmax.__all__+tan.__all__+tanh.__all__ +sigmoid.__all__+log.__all__)
